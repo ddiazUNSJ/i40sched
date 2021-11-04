3fea7723ad6095e7f44befc0ff07fa783d76904e
ddiazUNSJ <yddiaz@gmail.com>
2021-11-04 19:01:51 -0300

    jueves 4 de noviembre

--- server/lib/solver/solver.js
+++ server/lib/solver/solver.js
@@ -17,12 +17,15 @@ Meteor.methods({
 
   solve: function(files, req, res , idProblema , idSolJson , idSolLog){
     var attachments = [];
+    // Extrae del arreglo files los nombres de los archivos,con estos nombres construye 
+    // el arreglo de ficheros  "attachement" que contiene el contenido de archivos nombrados
     files.map(function (file) {
         attachments.push({
             name: file,
             stream: fs.createReadStream(`${Meteor.absolutePath}/modelsStore/` + file)
         });
     });
+    
     client.execute({
           logstream: process.stdout,
           parameters: {"oaas.TIME_LIMIT": 3 * 60 * 1000},
@@ -64,42 +67,7 @@ Meteor.methods({
 
 });
 
-Meteor.methods({
-  getList:function(){
-       if (client===undefined){
-        console.log("entrando por null");
-           client= iniciaSolver();
-           //console.log (client);
-       }
-       else{ console.log ("cliente ya instanciado");
-          //console.log (client);
-       }
-      var jobList = {};
-      //the job list from docplexcloud client api
-      var jobs = client.listJobs();
-      //process the promise output, to get the job ids
-      jobs.then(function (results) {
-          if (results.length === 0) {
-              console.log('Actualmente no se encuentran trabajos cargados');
-          }
-          else {
-              /*console.log('Lista de trabajos: ');
-              for (job = 0; job < results.length; job++) {
-                  console.log(results[job]._id);
-                  jobList[job] = results[job]._id;
-              }*/
-              if(results.length === 5){
-                 client.deleteJobs();
-              }else{
-                console.log("Espacio disponible en la nube para trabajar ("+ (5-results.length)+" lugar/es)");
-              }
-          }
-          //in case the response fails
-      }, function (err) {
-          console.log('job list ko' + err);
-    });
-  },
-});
+
 
 Meteor.methods({
       getSol:function(nameData, nameModel , idProblema , idSolJson , idSolLog){
@@ -138,6 +106,42 @@ Meteor.methods({
       }
 });
 
+Meteor.methods({
+  getList:function(){
+       if (client===undefined){
+        console.log("entrando por null");
+           client= iniciaSolver();
+           //console.log (client);
+       }
+       else{ console.log ("cliente ya instanciado");
+          //console.log (client);
+       }
+      var jobList = {};
+      //the job list from docplexcloud client api
+      var jobs = client.listJobs();
+      //process the promise output, to get the job ids
+      jobs.then(function (results) {
+          if (results.length === 0) {
+              console.log('Actualmente no se encuentran trabajos cargados');
+          }
+          else {
+              /*console.log('Lista de trabajos: ');
+              for (job = 0; job < results.length; job++) {
+                  console.log(results[job]._id);
+                  jobList[job] = results[job]._id;
+              }*/
+              if(results.length === 5){
+                 client.deleteJobs();
+              }else{
+                console.log("Espacio disponible en la nube para trabajar ("+ (5-results.length)+" lugar/es)");
+              }
+          }
+          //in case the response fails
+      }, function (err) {
+          console.log('job list ko' + err);
+    });
+  },
+});
 /*// Metodo que retorna el cliente SDK actual o crea uno sino existe y lo retorna
 Meteor.methods({
 
